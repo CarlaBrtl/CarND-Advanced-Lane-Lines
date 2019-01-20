3aee9b9c01a02fb6b3bb91d2be0a2d081c0fedf7
Carla Berteil <carla.brtl@gmail.com>
2019-01-20 22:28:55 +0100

    - Create an image with the lines  - Put it back into the original perspective  - Combine the original image with the image with the lines to see the    result

--- find_lanes.py
+++ find_lanes.py
@@ -93,7 +93,7 @@ def apply_thresholds(image):
     magnitude = np.sqrt(sobelx**2 + sobely**2)
     scaled_sobel_mag = np.uint8(255 * magnitude / np.max(magnitude))
     magniture_binary = np.zeros_like(magnitude)
-    magniture_binary[(scaled_sobel_mag >= 150) & (scaled_sobel_mag <= 300)] = 1
+    magniture_binary[(scaled_sobel_mag >= 50) & (scaled_sobel_mag <= 300)] = 1
 
     # Direction of the gradient
     direction = np.arctan2(absolute_sobely, absolute_sobelx)
@@ -119,7 +119,7 @@ def apply_thresholds(image):
 
     return combined
 
-def perspective_transform(image):
+def perspective_transform(image,  inverse):
     print "Transforming the perspective"
     img_size = (image.shape[1], image.shape[0])
     src = np.float32([[447, 548], [844, 548], [1005, 647], [301, 647]])
@@ -127,8 +127,12 @@ def perspective_transform(image):
     dst = np.float32([[301, 475], [1005, 475],
                       [1005, 647], [301, 647]])
 
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(image, M, img_size)
+    if inverse:
+        M = cv2.getPerspectiveTransform(dst, src)
+    else:
+        M = cv2.getPerspectiveTransform(src, dst)
+
+    warped = cv2.warpPerspective(image, M, img_size, flags=cv2.INTER_LINEAR)
 
     ## Note: Uncomment the following lines to test  on an image
     # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
@@ -141,10 +145,11 @@ def perspective_transform(image):
     return warped
 
 def detect_lines(image):
+    print "Detecting lines"
     shape = image.shape
 
     #Get the bottom third of the picture
-    number_of_windows = 5
+    number_of_windows = 100
     window_size = np.int(image.shape[0]//number_of_windows)
 
     left_peaks_y = []
@@ -154,28 +159,47 @@ def detect_lines(image):
 
     for window_id in range(number_of_windows):
 
-        # TODO clean this up
-        left_lane_point = detect_one_lane(image, left_peaks_x, shape, window_id, window_size, True)
-        left_peaks_x.append(left_lane_point[0])
-        left_peaks_y.append(left_lane_point[1])
-
-        right_lane_point = detect_one_lane(image, right_peaks_x, shape, window_id, window_size, False)
-        right_peaks_x.append(right_lane_point[0])
-        right_peaks_y.append(right_lane_point[1])
+        # Fill left_peaks_x and left_peaks_y with the points of the left lane
+        detect_one_lane(image, left_peaks_x, left_peaks_y, shape, window_id, window_size, True)
+        # Fill right_peaks_x and right_peaks_y with the points of the left lane
+        detect_one_lane(image, right_peaks_x, right_peaks_y, shape, window_id, window_size, False)
 
     left_fit = np.polyfit(left_peaks_y, left_peaks_x, 2)
     right_fit = np.polyfit(right_peaks_y, right_peaks_x, 2)
-    print left_fit
-    print right_fit
 
-    #TODO draw the line
+    out_img = draw_lines(image, left_fit, right_fit)
 
-def detect_one_lane(image, left_peaks_x, shape, window_id, window_size, isLeft):
+    return out_img
+
+
+def draw_lines(image, left_fit, right_fit):
+    # Assuming we have `left_fit` and `right_fit` from `np.polyfit` before
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, image.shape[0] - 1, image.shape[0])
+
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(image).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+    return color_warp
+
+def detect_one_lane(image, peaks_x, peaks_y, shape, window_id, window_size, isLeft):
     ## Left window
     window_bottom_y = int(shape[0] - (window_id + 1) * window_size)
     window_top_y = int(shape[0] - window_id * window_size)
 
-    if len(left_peaks_x) == 0:
+    if len(peaks_x) == 0:
         if isLeft:
             window_left_x = 0
             window_right_x = shape[1] // 2
@@ -183,8 +207,8 @@ def detect_one_lane(image, left_peaks_x, shape, window_id, window_size, isLeft):
             window_left_x = shape[1] // 2
             window_right_x = shape[1]
     else:
-        window_left_x = left_peaks_x[-1] - 100
-        window_right_x = left_peaks_x[-1] + 100
+        window_left_x = peaks_x[-1] - 100
+        window_right_x = peaks_x[-1] + 100
 
     # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
     # ax1.imshow(image)
@@ -195,19 +219,39 @@ def detect_one_lane(image, left_peaks_x, shape, window_id, window_size, isLeft):
 
     histogram = np.sum(image[window_bottom_y:window_top_y, :], axis=0)
 
-    # Find the 2 peaks, where the max of the histogram is
-    left_peak = np.argmax(histogram[window_left_x:window_right_x])
-    return left_peak + window_left_x, window_top_y
+    #  If we have more than 50 pixels, append to our peak list
+    if np.int(histogram[window_left_x:window_right_x].sum()) > 50:
+        left_peak = np.argmax(histogram[window_left_x:window_right_x])
+        peaks_x.append(left_peak + window_left_x)
+        peaks_y.append(window_top_y)
+
 
 # Run the code
+
 distortion_coefficients = calibrate()
 image_names = load_test_images_names()
 for image in image_names:
+    print "Starting the process"
     undistorted_image = undistort(image, distortion_coefficients)
     threshold_image = apply_thresholds(undistorted_image)
-    perspective_image = perspective_transform(threshold_image)
-    detect_lines(perspective_image)
+    perspective_image = perspective_transform(threshold_image, False)
+    lines_img = detect_lines(perspective_image)
+    # Put back into the original space
+    perspective_lines_img = perspective_transform(lines_img, True)
+
+    # Combine the result with the original image
+    result = cv2.addWeighted(undistorted_image, 1, perspective_lines_img, 0.3, 0)
+
+
+    ## Note: Uncomment the following lines to test  on an image
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+    ax1.imshow(undistorted_image)
+    ax1.set_title('Original Image', fontsize=30)
+    ax2.imshow(result)
+    ax2.set_title('Processed image', fontsize=30)
+    plt.show()
+
     # TODO calculate the turn
-    # TODO create an image without the perspective with the line
-    # TODO Put it in the image perspective
-    # TODO create a new image with both
+
+
+#TODO make it work on a video
